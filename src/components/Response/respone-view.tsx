8b1480ef7e019b3c9f41cd97c573eedc2758a1a5
Twarug <patryk@twardosz.dev>
2025-06-07 19:58:56 +0200

    Start final results data display (costs & deltas)

--- src/components/Response/respone-view.tsx
+++ src/components/Response/respone-view.tsx
@@ -1,20 +1,32 @@
-import { Card, CardTitle } from "../ui/card";
+import {Card, CardTitle} from "../ui/card";
 import type {ZZTResult} from "@/lib/zzt.ts";
 
 type ResponseViewProps = {
     result: ZZTResult;
 };
 
-export default function ResponseView({ result }: ResponseViewProps) {
-    return (
-        <Card className="flex grow flex-col max-h-[calc(100vh-16px)] items-center justify-center p-8">
-            <CardTitle className="text-2xl font-bold mb-4">Wyniki</CardTitle>
+export default function ResponseView({result}: ResponseViewProps) {
 
-            <div>
-                <h3 className="text-lg font-semibold mb-2">Dane ZZT:</h3>
-                <pre className="bg-gray-100 p-4 rounded-md text-black">
-                    {JSON.stringify(result, null, 2)}
-                </pre>
+    const totalProfitCalc = result.revenueTotal - (result.transportCostTotal + result.purchaseCostTotal);
+
+    return (
+        <Card className="flex grow flex-col max-h-[calc(100vh-16px)] items-center justify-center p-8 relative">
+            <CardTitle className="text-2xl font-bold mb-4">Wyniki</CardTitle>
+            <div className="absolute bottom-0 right-0 p-4 bg-black text-center flex gap-4 text-lg rounded-xl">
+                <p>
+                    {result.deltaReadable.map(p => <p>{p}</p>)}
+                </p>
+                <p>
+                    Zysk całkowity = {result.totalProfit} <br/>
+                    Koszt transportu = {result.transportCostTotal} <br/>
+                    Koszt zakupu = {result.purchaseCostTotal} <br/>
+                    Przychód = {result.revenueTotal} <br/>
+                    <br/>
+                    <p className={result.totalProfit == totalProfitCalc ? "text-green-400" : "text-red-600"}>
+                        {result.revenueTotal} - ({result.transportCostTotal} + {result.purchaseCostTotal})
+                        = {totalProfitCalc}
+                    </p>
+                </p>
             </div>
         </Card>
     );
